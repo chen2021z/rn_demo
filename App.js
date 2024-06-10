77afbdb259dff70268dfda97ff4a441ee84aac9f
Chen <2934570984@qq.com>
2024-06-10 13:23:25 +0800

    feat: 下拉刷新&上拉加载

--- App.js
+++ App.js
@@ -7,11 +7,17 @@ import {
   ActivityIndicator,
 } from "react-native";
 import { useState, useEffect } from "react";
-import { queryMovies } from "./data/Service";
+import { queryMovies, randomRefreshMovies } from "./data/Service";
 import MovieItemCell from "./components/MovieItemCell";
+import moviesData from "./data/movies.json";
 
 // 获取屏幕的宽度
 export const width = Dimensions.get("window").width;
+
+let currentPage = 1; // 当前页
+let pageSize = 10; // 每一页加载多少条
+let totalPage = Math.ceil(moviesData.length / pageSize); // 总页数
+
 
 export default function App() {
   const data = queryMovies();
@@ -19,19 +25,25 @@ export default function App() {
   const [movieList, setMovieList] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
+  // 新增一个状态
+  // 头部是否刷新
+  const [isHeaderRefreshing, setIsHeaderRefreshing] = useState(false);
+  const [isFooterRefreshing, setIsFooterRefreshing] = useState(false);
+
   // 模拟发送请求获取数据
   useEffect(() => {
     setTimeout(() => {
       setMovieList(data);
       setLoaded(true);
     }, 1000);
-  }, []);
+  },[]);
+
 
   // 渲染标题
   function renderTitle() {
     return (
       <View style={styles.barStyle}>
-        <Text style={styles.txtStyle}>电影列表1</Text>
+        <Text style={styles.txtStyle}>电影列表</Text>
       </View>
     );
   }
@@ -55,22 +67,68 @@ export default function App() {
     }
   }
 
+  // 下拉刷新对应的方法
+  function beginHeaderRefresh(){
+    // 下拉刷新所对应的行为
+    // 从服务器去获取新的数据
+    // 由于我们没有服务器，所以我们这里做一个模拟，随机返回两条数据
+    setIsHeaderRefreshing(true);
+    // 模拟刷新了两条
+    const newMovie = randomRefreshMovies();
+    const data = [...newMovie, ...movieList];
+    setTimeout(() => {
+      setMovieList(data);
+      setIsHeaderRefreshing(false);
+    }, 1000);
+  }
+
+  // 上拉加载对应的方法
+  function beginFooterRefresh(){
+    setIsFooterRefreshing(true);
+    if(currentPage < totalPage){
+      currentPage++;
+      const newMovie = queryMovies(currentPage, pageSize); // 查询对应页码的新数据
+      const data = [...movieList, ...newMovie];
+      setTimeout(() => {
+        setMovieList(data);
+        setIsFooterRefreshing(false);
+      }, 1000)
+    }
+  }
+
   // 渲染列表
   function renderList() {
     return (
-      <FlatList
+      <FlatList 
         data={movieList}
-        renderItem={({ item }) => (
-          <MovieItemCell
-            movie={item}
-            onPress={() => {
-              alert("点击的电影名：" + item.title);
-            }}
-          />
-        )}
-        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <MovieItemCell movie={item} onPress={()=>{
+          alert("点击的电影名：" + item.title);
+        }}/>}
+        keyExtractor={(item) => item.id + (new Date()).getTime() + Math.floor(Math.random() * 9999 + 1)}
+        refreshing={isHeaderRefreshing}
+        onRefresh={beginHeaderRefresh}
+        onEndReached={beginFooterRefresh}
+        onEndReachedThreshold={0.1}
       />
-    );
+    )
+  }
+
+  function renderFooterLoad(){
+    if(isFooterRefreshing){
+      return (
+        <View style={styles.footerStyle}>
+          <ActivityIndicator size="small" color="#268dcd" />
+            <Text
+              style={{
+                color: "#666",
+                paddingLeft: 10,
+              }}
+            >
+              努力加载中
+            </Text>
+        </View>
+      )
+    }
   }
 
   return (
@@ -81,6 +139,8 @@ export default function App() {
       {renderLoad()}
       {/* 渲染列表 */}
       {renderList()}
+      {/* 渲染底部加载条 */}
+      {renderFooterLoad()}
     </View>
   );
 }
@@ -107,5 +167,12 @@ const styles = StyleSheet.create({
     color: "#fff",
     textAlign: "center",
     fontSize: 18,
+  },
+  footerStyle: {
+    height: 48,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
   },
 });
