e944c00e8a2eec1b8cb6510387b748ad1df228c8
Chen <2934570984@qq.com>
2024-06-21 21:29:23 +0800

    feat: 使用 native-stack

--- App.js
+++ App.js
@@ -1,41 +1,31 @@
-import React, { PureComponent } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import RadioGroup from "./components/RadioGroup";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data:[{ text: "个人" }, { text: "单位" }, { text: "其他" }]
-    };
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>发票抬头</Text>
-        <RadioGroup
-          orientation="column"
-          data={this.state.data}
-          defaultValue={1} //默认选中的值
-          drawablePadding={10} //图片与文字的间距
-          itemChange={(index) => {
-            alert(index);
+import HomeScreen from "./views/HomeScreen"
+import DetailScreen from "./views/DetailScreen"
+
+// 创建一个栈导航器
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            title: "主页1"
           }}
-        ></RadioGroup>
-      </View>
-    );
-  }
+        />
+        <Stack.Screen 
+          name="DetailScreen"
+          component={DetailScreen}
+          options={{
+            title: "详情页"
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 35,
-  },
-  title: {
-    height : 30,
-    fontSize: 20,
-    borderColor: "black",
-    marginLeft: 15,
-    fontWeight: "bold",
-  },
-});
