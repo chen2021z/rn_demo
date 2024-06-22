dd1c68378a12bb885b934054819efd9c82ccbdb3
Chen <2934570984@qq.com>
2024-06-22 21:28:51 +0800

    feat：测试其他类型的导航

--- App.js
+++ App.js
@@ -1,36 +1,75 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+// import { Ionicons } from '@expo/vector-icons'
+import AntDesign from '@expo/vector-icons/AntDesign'
 
-import HomeScreen from "./views/HomeScreen";
-import DetailScreen from "./views/DetailScreen";
-import SettingScreen from "./views/SettingScreen";
-import ProfileScreen from "./views/ProfileScreen";
+import HomeScreen from './views/HomeScreen'
+import DetailScreen from './views/DetailScreen'
+import SettingScreen from './views/SettingScreen'
+import ProfileScreen from './views/ProfileScreen'
+import GameScreen from './views/GameScreen'
+import MovieScreen from './views/MovieScreen'
 
 // 创建一个栈导航器
-const HomeStack = createNativeStackNavigator();
-const SettingStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator()
+const SettingStack = createNativeStackNavigator()
 
 // 创建一个标签页导航
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
+const topTab = createMaterialTopTabNavigator()
+
+// 创建一个抽屉导航器
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        {/* 第一个标签页 */}
-        <Tab.Screen name="First">
-          {/* 里面嵌套一个栈导航器 */}
+      <Drawer.Navigator initialRouteName="HomeScreen">
+        <Drawer.Screen name="主页" component={HomeScreen}></Drawer.Screen>
+        <Drawer.Screen name="详情页" component={DetailScreen} initialParams={{
+          id : 1,
+          name : "xiejie",
+          age : 18
+        }}></Drawer.Screen>
+      </Drawer.Navigator>
+      {/* <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconname = null
+
+            if (route.name === 'First') {
+              iconname = focused ? 'pay-circle1' : 'pay-circle-o1'
+            } else if (route.name === 'Second') {
+              iconname = focused ? 'pluscircle' : 'pluscircleo'
+            }
+
+            return <AntDesign name={iconname} size={size} color={color} />
+          },
+        })}
+      >
+        <Tab.Screen
+          name="First"
+          options={{
+            tabBarBadge: 101,
+          }}
+        >
           {() => (
-            <HomeStack.Navigator>
-              <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
-              <HomeStack.Screen name="DetailScreen" component={DetailScreen} />
-            </HomeStack.Navigator>
+            // <HomeStack.Navigator>
+            //   <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
+            //   <HomeStack.Screen name="DetailScreen" component={DetailScreen} />
+            // </HomeStack.Navigator>
+            <topTab.Navigator>
+              <topTab.Screen name="Game" component={GameScreen} />
+              <topTab.Screen name="Movie" component={MovieScreen} />
+            </topTab.Navigator>
           )}
         </Tab.Screen>
-        {/* 第二个标签页 */}
         <Tab.Screen name="Second">
-          {/* 里面嵌套一个栈导航器 */}
           {() => (
             <SettingStack.Navigator>
               <SettingStack.Screen
@@ -44,7 +83,11 @@ export default function App() {
             </SettingStack.Navigator>
           )}
         </Tab.Screen>
-      </Tab.Navigator>
+      </Tab.Navigator> */}
+      {/* <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingScreen} />
+      </Tab.Navigator> */}
     </NavigationContainer>
-  );
+  )
 }
